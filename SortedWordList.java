ada8f3c5afe3b5006d126d08c8db295914dedc19
Ashir Qureshi <160384665+Aqureshi106@users.noreply.github.com>
2024-04-26 18:52:32 -0400

    Create SortedWordList.java

---
+++ SortedWordList.java
@@ -0,0 +1,15 @@
+package project3;
+
+/**
+ * The SortedWordList class represents a list of words in a sorted manner.
+ * It extends the AbstractWordList class.
+ */
+public class SortedWordList extends AbstractWordList {
+	/**
+     * Constructor for the SortedWordList class.
+     * Invokes the superclass AbstractWordList constructor.
+     */
+    public SortedWordList() { // A constructor that invokes the superclass WordList.
+        super();
+    }
+}
